275d40234a2f8d485e0bc7d70d35c62398da39b8
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2023-01-23 18:39:55 +0100

    Completed + if on title condition added

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -16,21 +16,21 @@
 </head>
 
 <body>
-    <h1 class="display-5 fw-bold text-center">
+    <h1 class="display-5 fw-bold text-center mt-3">
        Welcome to Laravel_Model_Controller
    </h1>
-    <div class="container d-flex flex-wrap">
-            @php
-                // dump($movies);
-            @endphp
+    <div class="container-fluid d-flex justify-content-center flex-wrap w90 mt-2">
             @foreach ($movies as $movie) 
-                <div class="card mx-2 my-2" style="width: 18rem;">
+                <div class="card mx-1 my-2 color" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">({{$movie->id}}) {{$movie->title}} </h5>
-                        <h5 class="card-title">Titolo originale: {{$movie->original_title}} </h5>
-                        <p class="card-text">Nationalità: {{$movie->nationality}}</p>
-                        <p class="card-text">Data di uscita: {{$movie->date}}</p>
-                        <p class="card-text">Valutazione: {{$movie->vote}}</p>
+                        <h5 class="card-title">Titolo: ({{$movie->id}}) {{$movie->title}} </h5>
+                         @if ($movie->original_title === $movie->title)
+                        @else  
+                            <h5 class="card-title">Titolo originale: {{$movie->original_title}} </h5>    
+                         @endif
+                        <p class="card-text">Nationalità: <b>{{$movie->nationality}}</b></p>
+                        <p class="card-text">Data di uscita: <b>{{$movie->date}}</b></p>
+                        <p class="card-text">Valutazione: <b>{{$movie->vote}}</b></p>
                     </div>
                 </div>    
             @endforeach
